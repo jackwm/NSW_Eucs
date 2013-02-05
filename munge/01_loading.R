8be6041e25845e05e419b6e0c8fa10d7437780e2
Jack Moon <jack@contraflo.ws>
2013-02-05 15:03:30 +1100

    + fnc error corrections source

--- munge/01_loading.R
+++ munge/01_loading.R
@@ -5,6 +5,7 @@ source('src/functions_date.R')
 source('src/functions_max_min.R')
 source('src/functions_SEI.R')
 source('src/functions_pre_processing.R')
+source('src/functions_error_corrections.R')
 
 if (!exists('data.dir')) {
   print('No selected data directory, please select a date. Later dates will contain more data')
