a4b3c4cab8bdd5fa3b3ed5c8667006a234559dbe
Jack Moon <jack@contraflo.ws>
2013-02-05 14:30:53 +1100

    + error fixing not finished

--- munge/04_data_correction.R
+++ munge/04_data_correction.R
@@ -1,4 +1,12 @@
 
+  # Correcting spikes
+  for (n in c(1:nrow(errors))){
+    if (errors[n,'error.type']=='spike'){
+      
+    }else print(errors[n,'error.type'])
+  }
+
+  if (FALSE) {
     # plotting some interesting parts, to do with the tree swap
     ############################################################
     t.tree <- 'Dendro_Avg.4.03F'
@@ -44,41 +52,6 @@
     # Sap.All[1:11862,13]<-Sap.All[1:11862,13]+correction
     # This was a tree swap. Need to make a new column and move the preceeding data there. 
     ############################################################
-    
-    
-    # creating df of 'spike' errors to loop over
-    spikes.df <- data.frame(e.time,e.tree)
-    # running corrections on the spikes errors df
-    for (i in 1:nrow(spikes.df)) print(spikes.df[i,'x'])
-    for (i in 1:nrow(spikes.df)) {  
-      e.time <- spikes.df[i, 'e.time']
-      e.time <- as.POSIXct(e.time)
-      e.tree <- spikes.df[i, 'y']
-      e.tree <- as.character(e.tree)
-      Sap.All[ Sap.All$TIMESTAMP == e.time, e.tree ] <- ( Sap.All[ Sap.All$TIMESTAMP == (x-10*60), e.tree ] + Sap.All[ Sap.All$TIMESTAMP == (x+10*60), y] ) / 2
-    }
-    
-    # creating df of 're-adjustment' errors to loop over
-    readju.df <- data.frame(u,w)
-    # running corrections on the readjument errors df. 
-    for (i in 1:nrow(readju.df)) print(readju.df[i,c('u','w')])
-    
-    for (i in 1:nrow(readju.df)) {  
-      u <- readju.df[i, 'u']
-      u <- as.POSIXct(u)
-      w <- readju.df[i, 'w']
-      w <- as.character(w)
-      # re-adjusting
-      # de.tree is delta e.tree, the difference between the two periods. 
-      de.tree <- Sap.All[ Sap.All$TIMESTAMP == (u+10*60), w] - Sap.All[ Sap.All$TIMESTAMP == (u-10*60), w]
-      Sap.All[ Sap.All$TIMESTAMP < u, w] <- Sap.All[ Sap.All$TIMESTAMP < u, w ] + dy
-      # fixing 'spike' between the two period which also needs to be removed.
-      Sap.All[ Sap.All$TIMESTAMP == u, w ] <- ( Sap.All[ Sap.All$TIMESTAMP == (u-10*60), w ] + Sap.All[ Sap.All$TIMESTAMP == (u+10*60), w ] ) / 2
-    }
-    
-    tree.names <- TreeInfo(Sap.All) # might be worth adding the columns "site" and "tree" here. 
-    trees <- GetTrees(Sap.All)
-    trees.short <- c("t1s1","t2s1","t3s1","t4s1","t1s2","t2s2","t3s2","t4s2","t1s3","t2s3","t3s3","t4s4")
-    
-    # cleaning up
-    rm(i,nam,my.col.names.obj,MyReadFunc,file.dir,my.file.list,my.obj.list)
+  }
+  # cleaning up
+  rm(i,nam,my.col.names.obj,MyReadFunc,file.dir,my.file.list,my.obj.list)
--- src/functions_error_corrections.R
+++ src/functions_error_corrections.R
@@ -1,3 +1,8 @@
-SpikeCorrection <- function(df,tree,time){
-  
+SpikeCorrection <- function(df,tree,time,n){
+  result <- df
+  sub <- subset(df,df$TIMESTAMP>=time-10*60*n)
+  sub <- subset(sub,sub$TIMESTAMP<=time+10*60*n)
+  avg <- sum(c(sub[1:n,tree],sub[(n+2):(n*2+1),tree]))/(2*n)
+  result[result$TIMESTAMP==time,tree] <- avg
+  return(result)
 }
