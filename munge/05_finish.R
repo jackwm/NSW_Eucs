a55a4d3ae2a6c4e621157d170a8c56abbc318b69
Jack Moon <jack@contraflo.ws>
2013-02-09 13:37:31 +1100

    + removing automatic csv creation

--- munge/05_finish.R
+++ munge/05_finish.R
@@ -1,13 +1,15 @@
 # writing all the data frames to csv's and storing in csv folder
 cnt <- 0;
-for (df in df.list){
-  cnt <- cnt + 1
-  write.csv(df,paste('csv/',names(df.list)[cnt],'.csv',sep=''),row.names=FALSE)
+if (changes.made){
+  for (df in df.list){
+    cnt <- cnt + 1
+    write.csv(df,paste('csv/',names(df.list)[cnt],'.csv',sep=''),row.names=FALSE)
+  }
+  rm(df)
 }
-
 # Calculating time taken
 time.taken <- round(as.numeric(difftime(now(),start,units='secs')),2)
 print(paste('Time taken to load:',time.taken,'secs'))
 
 # Cleaning up unused variables
-rm(start,time.taken,cnt,df)
+rm(start,time.taken,cnt)
