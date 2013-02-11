96bd099daedf6773997891b90f4068abd4f20ad0
Jack Moon <jack@contraflo.ws>
2013-02-11 12:43:42 +1100

    - skipped the incorrect statement till ready to fix

--- munge/05_fill_missing_obs.R
+++ munge/05_fill_missing_obs.R
@@ -1,6 +1,8 @@
 # 05_fill_missing_obs
 # In case we still need to fill the missing observations with NAs
 
-for (i in df.list){
-df.list[[i]] <- MergeSynth(df.list[[i]], 600)
+if (FALSE){
+  for (i in df.list){
+    df.list[[i]] <- MergeSynth(df.list[[i]], 600)
+  }
 }
