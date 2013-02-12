18212a93ef67eacc505fcce16daee4b4ce42fba3
Stephen Fujiwara <fuji@Stephens-MacBook-Air.local>
2013-02-12 14:12:15 +1100

    in the MergeSynth call, changed the for loop to use a number in df.list

--- munge/05_fill_missing_obs.R
+++ munge/05_fill_missing_obs.R
@@ -1,8 +1,8 @@
 # 05_fill_missing_obs
 # In case we still need to fill the missing observations with NAs
 
-if (FALSE){
+if (TRUE){
   for (i in df.list){
-    df.list[[i]] <- MergeSynth(df.list[[i]], 600)
+    i <- MergeSynth(i, 600)
   }
 }
